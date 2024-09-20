b107f231f339ce20bab26deec64bd34d5759a6e1
alexandre11aa <alexandrenobrega@ec.fiponline.edu.br>
2024-09-20 16:29:13 -0300

    20.09.2024-4

--- api_model_chat/chat/views.py
+++ api_model_chat/chat/views.py
@@ -7,24 +7,25 @@ from .models import Message
 
 # Redireciona para a lista de usuários após o login
 class LoginView(AuthLoginView):
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('user_list')  # Redireciona para a lista de usuários
+        return super().get(request, *args, **kwargs)
+
     def get_success_url(self):
         return reverse('user_list')
 
 def user_list(request):
     users = CustomUser.objects.all()  # Obtém todos os usuários
-    return render(request, 'userList.html', {'users': users})
-
-def chat_view(request, target_code):
-    target_user = get_object_or_404(CustomUser, code=target_code)
-    return render(request, 'chatPage.html', {'target_user': target_user})
+    logged_user = request.user  # Usuário logado
+    return render(request, 'userList.html', {'users': users, 'logged_user': logged_user})
 
 def chat_view(request, code):
     if not request.user.is_authenticated:
         return redirect("login-user")
-    
+
     target_user = get_object_or_404(CustomUser, code=code)
 
-    # Recupera as mensagens trocadas entre o usuário logado e o usuário alvo
     messages = Message.objects.filter(
         Q(sender=request.user, receiver=target_user) |
         Q(sender=target_user, receiver=request.user)
@@ -33,6 +34,7 @@ def chat_view(request, code):
     context = {
         'target_user': target_user,
         'messages': messages,
+        'logged_user': request.user,
     }
 
     return render(request, 'chatPage.html', context)
