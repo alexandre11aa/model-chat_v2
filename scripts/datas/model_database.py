72e11a0f932850aa12b34d2ef0f47b6388ea623b
Alexandre Estrela <82901702+alexandre11aa@users.noreply.github.com>
2024-09-26 16:39:54 -0300

    26.09.2024-05

--- scripts/datas/model_database.py
+++ scripts/datas/model_database.py
@@ -1,45 +1,75 @@
 from consumer.models import CustomUser
+from django.core.exceptions import ObjectDoesNotExist
+
+def create_user_if_not_exists(name, email, password, matricula, sexo, user):
+
+    # Tenta obter o usuário pelo número de matrícula
+    try:    
+        existing_user = CustomUser.objects.get(matricula=matricula)
+        return existing_user
+    
+    # Se não existir, cria um novo usuário ou super-usuário    
+    except ObjectDoesNotExist:
+
+        if user == 's':
+            tipo_de_usuario = CustomUser.objects.create_superuser
+            
+        elif user == 'u':
+            tipo_de_usuario = CustomUser.objects.create_user
+        
+        return tipo_de_usuario(
+                name=name,
+                email=email,
+                password=password,
+                matricula=matricula,
+                sexo=sexo
+            )
 
 # Crie um superuser
-superuser = CustomUser.objects.create_superuser(
+superuser = create_user_if_not_exists(
     name='admin',
     email='admin@example.com',
     password='admin',
     matricula=1000,
-    sexo='M'
+    sexo='M',
+    user='s'
 )
 
 # Crie outros usuários
-user1 = CustomUser.objects.create_user(
+user1 = create_user_if_not_exists(
     name='Joao',
     email='joao@example.com',
     password='senha123',
     matricula=1001,
-    sexo='M'
+    sexo='M',
+    user='u'
 )
 
-user2 = CustomUser.objects.create_user(
+user2 = create_user_if_not_exists(
     name='Maria',
     email='maria@example.com',
     password='senha123',
     matricula=1002,
-    sexo='F'
+    sexo='F',
+    user='u'
 )
 
-user3 = CustomUser.objects.create_user(
+user3 = create_user_if_not_exists(
     name='Pedro',
     email='pedro@example.com',
     password='senha123',
     matricula=1003,
-    sexo='O'
+    sexo='O',
+    user='u'
 )
 
-user4 = CustomUser.objects.create_user(
+user4 = create_user_if_not_exists(
     name='Ana',
     email='ana@example.com',
     password='senha123',
     matricula=1004,
-    sexo='N'
+    sexo='N',
+    user='u'
 )
 
 exit()
