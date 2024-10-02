bb85c14012a78cd4bcdbd98057a1e098b7f46c87
alexandre11aa <alexandrenobrega@ec.fiponline.edu.br>
2024-10-02 17:32:26 -0300

    02.10.2024-1

--- model_chat/chat/admin.py
+++ model_chat/chat/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
-from .models import DuoMessage
+from .models import DuoMessage, DuoFile
 
 @admin.register(DuoMessage)
 class DuoMessageAdmin(admin.ModelAdmin):
-    list_display = ('sender', 'receiver', 'message', 'file', 'timestamp')
+    list_display = ('sender', 'receiver', 'message', 'timestamp')
+    search_fields = ('sender__name', 'receiver__name', 'message')
+    list_filter = ('timestamp', 'sender', 'receiver')
+    ordering = ('-timestamp',)
+    list_per_page = 20
+
+@admin.register(DuoFile)
+class DuoMessageAdmin(admin.ModelAdmin):
+    list_display = ('sender', 'receiver', 'file', 'timestamp')
     search_fields = ('sender__name', 'receiver__name', 'message')
     list_filter = ('timestamp', 'sender', 'receiver')
     ordering = ('-timestamp',)
--- model_chat/chat/models.py
+++ model_chat/chat/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 from consumer.models import CustomUser, BaseModel
 
-class DuoMessage(BaseModel):
-    sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='sent_messages')
-    receiver = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='received_messages')
-    message = models.TextField()
-    file = models.FileField(upload_to='uploads/', blank=True, null=True)
+class Duo(BaseModel):
+    sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='%(class)s_sent_messages')
+    receiver = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='%(class)s_received_messages')
     timestamp = models.DateTimeField(auto_now_add=True)
-    is_read = models.BooleanField(default=False) 
+    is_read = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+class DuoMessage(Duo):
+    message = models.TextField()
 
     def __str__(self):
         return f'Message from {self.sender.name} to {self.receiver.name}'
+
+class DuoFile(Duo):
+    file = models.FileField(upload_to='uploads/', blank=True, null=True)
+
+    def __str__(self):
+        return f'File from {self.sender.name} to {self.receiver.name}'
