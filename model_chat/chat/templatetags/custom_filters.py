5b664abbbf6f32d227a2b96038cacfe278914ebf
alexandre11aa <alexandrestrela@hotmail.com>
2024-10-16 21:20:32 -0300

    16.10.2024-5

--- model_chat/chat/templatetags/custom_filters.py
+++ model_chat/chat/templatetags/custom_filters.py
@@ -5,3 +5,8 @@ register = template.Library()
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+@register.filter
+def endswith(value, arg):
+    """Verifica se a string termina com a extensão fornecida."""
+    return value.endswith(arg)
