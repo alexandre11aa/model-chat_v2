87d953ffa2ce82dee63b56c18e614026651ff1d1
alexandre11aa <alexandrestrela@hotmail.com>
2025-01-15 16:56:26 -0300

    consertando problemas de rotas

--- backend/model-chat/core/settings.py
+++ backend/model-chat/core/settings.py
@@ -32,9 +32,9 @@ ALLOWED_HOSTS = ['*']  # Recomendado apenas para teste
 
 CURRENT_URL = 'http://127.0.0.1:8000/'
 
-CORS_ALLOWED_ORIGINS = [
-    'http://172.20.0.2:3000'  # IP usado pelo Frontend
-]
+CURRENT_URL = os.getenv('CURRENT_URL', 'http://127.0.0.1:8000/')
+
+CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', '').split(',')
 
 
 # Application definition
